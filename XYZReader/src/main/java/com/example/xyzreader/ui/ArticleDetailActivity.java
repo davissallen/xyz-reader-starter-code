1b25d45cada6bb69f8bf80d52e8cfe7b87c7c572
Davis Allen <davissallen@gmail.com>
2017-09-05 15:25:18 -0700

    Hide status bar on detail screen

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
@@ -47,11 +48,6 @@ public class ArticleDetailActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
 
         setContentView(R.layout.activity_article_detail);
 
@@ -62,13 +58,8 @@ public class ArticleDetailActivity extends AppCompatActivity
         if (savedInstanceState == null) {
             Intent receivedIntent = getIntent();
             mInitialPosition = receivedIntent.getIntExtra(EXTRA_INITIAL_POSITION, 0);
-            // mArticles = receivedIntent.getParcelableArrayListExtra(EXTRA_ARTICLES);
             mInitialPhotoUrl = receivedIntent.getStringExtra(EXTRA_CLICKED_IMAGE_URL);
         }
-
-        // TODO: Update initial photo src
-
-        // TODO: Hide top status bar
 
         mUpButtonContainer = findViewById(R.id.up_container);
         mUpButton = findViewById(R.id.action_up);
@@ -82,21 +73,16 @@ public class ArticleDetailActivity extends AppCompatActivity
         mPagerAdapter = new MyPagerAdapter(getFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(mPagerAdapter);
-        // TODO: Do I need an onpagechangelistener to change the item..?
-//        mPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//                super.onPageScrollStateChanged(state);
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                if (mCursor != null) {
-//                    mCursor.moveToPosition(position);
-//                }
-//            }
-//        });
         mPager.setCurrentItem(mInitialPosition);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
     }
 
     @Override
